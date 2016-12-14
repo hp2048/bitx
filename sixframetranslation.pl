219425d8c4759f0baf2a6dcd1e60766f93cc46a2
Hardip Patel <patelhardip@gmail.com>
2016-12-14 11:32:50 +1100

    Update sixframetranslation.pl

--- sixframetranslation.pl
+++ sixframetranslation.pl
@@ -1,114 +1,92 @@
 #!/usr/bin/perl -w
 use strict;
-my ($infile, $outfile) = @ARGV;
-open (IN, "<$infile") or die $!;
-open (OUT, ">$outfile") or die $!;
-my $seq = "";
-my $header = "";
-while (<IN>){
-  chomp $_;
-  if ($_ =~ />(.*)/){
-	 if (length($seq)>0){
-		for (my $i=-3;$i<=3;$i++){
-		  if ($i != 0){
-			 my $translation = get_translation($seq, $i);
-			 print OUT ">$header".":$i\n";
-			 print OUT "$translation\n";
-		  }
-		}
-	 }
-	 $header = $1;
-	 $seq = "";
-  }else{
-	 $seq .= $_;
+my ($infile, $outbase, $minLen, $maxLen) = @ARGV;
+
+open (ALL, ">$outbase.all.fasta") or die $!;
+open (ORF, ">$outbase.orf.fasta") or die $!;
+my $seqs = readFasta($infile);
+
+foreach my $seqcounter (sort {$a<=>$b} keys %$seqs){
+  foreach my $frame (1,2,3,-1,-2,-3){ 
+	my $translation = get_translation($$seqs{$seqcounter}{'sequence'}, $frame);
+	
+	print ALL ">$$seqs{$seqcounter}{'id'}".":$frame\n";
+	print ALL "$translation\n";
+	
+	while ($translation=~/(M\w{$minLen,$maxLen}\*)/g){
+     my $orf   = $1;
+     my $start = (pos($translation) - length($orf)) * 3 + 1;
+     my $end   = pos($translation) * 3;
+     $start = $start + abs($frame) - 1;
+     $end   = $end   + abs($frame) - 1;
+     
+     print ORF ">$$seqs{$seqcounter}{'id'}".":$frame:$start:$end\n$orf\n";
+	 pos($translation)=pos($translation)-length($orf)+1; 
+	 ###this sets the pos so the next while iteration starts searching from after the detected methionine in this loop
+	 ###eg. MAMAC* will be reported as MAMAC* and MAC* as well, if pos is not set then only MAMAC* gets reported
+    } 
   }
 }
-if (length($seq)>0){
-  for (my $i=-3;$i<=3;$i++){
-	 if ($i != 0){
-		my $translation = get_translation($seq, $i);
-		print OUT ">$header".":$i\n";
-		print OUT "$seq\n";
-	 }
+
+
+sub readFasta {
+  my ($f) = @_;
+  my $counter = 0;
+  my %seqs = ();
+  open (FASTA, "<$f") or die $!;
+  my $id = "";
+  while (<FASTA>){
+    chomp $_;
+    if ($_ =~ />\s*(\S+)\s*(.*)/){
+      $id = $1;
+      my $description = $2;
+ 	  $counter++;
+      $seqs{$counter}{'id'} = $id;
+      $seqs{$counter}{'description'} = $description;
+    }
+    else{
+      $seqs{$counter}{'sequence'} .= $_;
+    }
   }
+  close FASTA;
+  return \%seqs;
 }
+
 sub get_translation {
   my %genetic_code  = (
-  'TCA' => 'S',
-  'TCC' => 'S',
-  'TCG' => 'S',
-  'TCT' => 'S',
-  'TTC' => 'F',
-  'TTT' => 'F',
-  'TTA' => 'L',
-  'TTG' => 'L',
-  'TAC' => 'Y',
-  'TAT' => 'Y',
-  'TAA' => '*',
-  'TAG' => '*',
-  'TGC' => 'C',
-  'TGT' => 'C',
-  'TGA' => '*',
-  'TGG' => 'W',
-  'CTA' => 'L',
-  'CTC' => 'L',
-  'CTG' => 'L',
-  'CTT' => 'L',
-  'CCA' => 'P',
-  'CAT' => 'H',
-  'CAA' => 'Q',
-  'CAG' => 'Q',
-  'CGA' => 'R',
-  'CGC' => 'R',
-  'CGG' => 'R',
-  'CGT' => 'R',
-  'ATA' => 'I',
-  'ATC' => 'I',
-  'ATT' => 'I',
+  'GCA' => 'A',  'GCC' => 'A',  'GCG' => 'A',  'GCT' => 'A',
+  'TGC' => 'C',  'TGT' => 'C',
+  'GAC' => 'D',  'GAT' => 'D',
+  'GAA' => 'E',  'GAG' => 'E',
+  'TTC' => 'F',  'TTT' => 'F',
+  'GGA' => 'G',  'GGC' => 'G',  'GGG' => 'G',  'GGT' => 'G',
+  'CAT' => 'H',  'CAC' => 'H',
+  'ATA' => 'I',  'ATC' => 'I',  'ATT' => 'I',
+  'AAA' => 'K',  'AAG' => 'K',
+  'TTA' => 'L',  'TTG' => 'L',  'CTA' => 'L',  'CTC' => 'L',  'CTG' => 'L',  'CTT' => 'L',
   'ATG' => 'M',
-  'ACA' => 'T',
-  'ACC' => 'T',
-  'ACG' => 'T',
-  'ACT' => 'T',
-  'AAC' => 'N',
-  'AAT' => 'N',
-  'AAA' => 'K',
-  'AAG' => 'K',
-  'AGC' => 'S',
-  'AGT' => 'S',
-  'AGA' => 'R',
-  'AGG' => 'R',
-  'CCC' => 'P',
-  'CCG' => 'P',
-  'CCT' => 'P',
-  'CAC' => 'H',
-  'GTA' => 'V',
-  'GTC' => 'V',
-  'GTG' => 'V',
-  'GTT' => 'V',
-  'GCA' => 'A',
-  'GCC' => 'A',
-  'GCG' => 'A',
-  'GCT' => 'A',
-  'GAC' => 'D',
-  'GAT' => 'D',
-  'GAA' => 'E',
-  'GAG' => 'E',
-  'GGA' => 'G',
-  'GGC' => 'G',
-  'GGG' => 'G',
-  'GGT' => 'G',
+  'AAC' => 'N',  'AAT' => 'N',
+  'CCA' => 'P',  'CCC' => 'P',  'CCG' => 'P',  'CCT' => 'P',
+  'CAA' => 'Q',  'CAG' => 'Q',
+  'CGA' => 'R',  'CGC' => 'R',  'CGG' => 'R',  'CGT' => 'R',  'AGA' => 'R',  'AGG' => 'R',
+  'TCA' => 'S',  'TCC' => 'S',  'TCG' => 'S',  'TCT' => 'S',  'AGC' => 'S',  'AGT' => 'S',
+  'ACA' => 'T',  'ACC' => 'T',  'ACG' => 'T',  'ACT' => 'T',
+  'GTA' => 'V',  'GTC' => 'V',  'GTG' => 'V',  'GTT' => 'V',
+  'TGG' => 'W',
+  'TAC' => 'Y',	 'TAT' => 'Y',
+  'TAA' => '*',  'TAG' => '*',  'TGA' => '*',
   );
   
   my $seq = shift;
   my $frame = shift;
   my $translation = "";
+  $seq = uc($seq);
   if ($frame < 0){
 	 $seq = reverse($seq);
 	 $seq =~ tr/ACGT/TGCA/;
   }
   
-  for (my $i=(abs($frame)-1);$i<length($seq)-3;$i+=3){
+  for (my $i=(abs($frame)-1);$i<=length($seq)-3;$i+=3){
 	 $translation .= $genetic_code{substr($seq, $i, 3)};
   }
   return $translation;
